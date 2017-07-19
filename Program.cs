2a452c65f2b41531312154fc6bf020bc1baeb0ea
Stan Santiago <stsantia@microsoft.com>
2017-07-19 17:53:47 -0400

    first commit

--- Program.cs
+++ Program.cs
@@ -4,12 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Threading;
+using Microsoft.ServiceBus.Messaging;
+
 namespace RedEyeSender
 {
     class Program
     {
+
+        
+
+        static string eventHubName = "stansevents-eh";
+        static string connectionString = "Endpoint=sb://stansevents.servicebus.windows.net/;SharedAccessKeyName=stanevents-policy-manage;SharedAccessKey=gI7HUiQBKht8LsohSGpXcu5mvdfxIjhLx3jKyBStKeA=";
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Press Ctrl-C to stop the sender process");
+            Console.WriteLine("Press Enter to start now");
+            Console.ReadLine();
+            SendingRandomMessages();
+        }
+
+        static void SendingRandomMessages()
+        {
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, eventHubName);
+            while (true)
+            {
+                try
+                {
+                    var message = Guid.NewGuid().ToString();
+                    Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, message);
+                    eventHubClient.Send(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0} > Exception: {1}", DateTime.Now, exception.Message);
+                    Console.ResetColor();
+                }
+
+                Thread.Sleep(200);
+            }
+        
         }
     }
 }
