c095cb35cb17ea53c5cebbdf7d9a49aad21ec699
Stan Santiago <stsantia@microsoft.com>
2017-07-19 21:04:49 -0400

    added json message

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using System.Threading;
 using Microsoft.ServiceBus.Messaging;
+using System.Dynamic;
 
 namespace RedEyeSender
 {
@@ -15,7 +16,7 @@ namespace RedEyeSender
         
 
         static string eventHubName = "stansevents-eh";
-        static string connectionString = "Endpoint=sb://stansevents.servicebus.windows.net/;SharedAccessKeyName=stanevents-policy-manage;SharedAccessKey=gI7HUiQBKht8LsohSGpXcu5mvdfxIjhLx3jKyBStKeA=";
+        static string connectionString = "";
 
         static void Main(string[] args)
         {
@@ -28,14 +29,51 @@ namespace RedEyeSender
         static void SendingRandomMessages()
         {
             var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, eventHubName);
-            while (true)
-            {
+           
                 try
                 {
-                    var message = Guid.NewGuid().ToString();
+                    dynamic redye = new ExpandoObject();
+
+                    redye.has_passed = "something";
+                    redye.total_no_of_cheeses = "something";
+                    redye.total_batch_weight = "something";
+                    redye.thread_quality = "something";
+                    redye.group_55 = "something";
+                    redye.fibre_mix = "something";
+                    redye.L_value = "something";
+                    redye.A_value = "something";
+                    redye.B_value = "something";
+                    redye.chroma_value = "something";
+                    redye.hue_value = "something";
+                    redye.delta_l = "something";
+                    redye.delta_c = "something";
+                    redye.delta_h = "something";
+                    redye.max_colour_difference = "something";
+                    redye.batch_id = "something";
+                    redye.substrate_code = "something";
+                    redye.count_ply = "something";
+                    redye.recipe_type = "something";
+                    redye.colour_category = "something";
+                    redye.finish_type = "something";
+                    redye.fastness_type = "something";
+                    redye.dyeing_method = "something";
+                    redye.Machine_volume = "something";
+                    redye.material_code = "something";
+                    redye.fibre_type = "something";
+                    redye.dye_classes = "something";
+                    redye.parent_batch_id = "something";
+
+
+                    string json = Newtonsoft.Json.JsonConvert.SerializeObject(redye);
+                    //var message = Guid.NewGuid().ToString();
+                    var message = json;
+
                     Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, message);
                     eventHubClient.Send(new EventData(Encoding.UTF8.GetBytes(message)));
-                }
+
+                    Console.WriteLine("Press Enter to end now");
+                    Console.ReadLine();
+            }
                 catch (Exception exception)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
@@ -43,8 +81,7 @@ namespace RedEyeSender
                     Console.ResetColor();
                 }
 
-                Thread.Sleep(200);
-            }
+               
         
         }
     }
